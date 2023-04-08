9ead95f793cf3b8e22ee01e7d26d9631ce22cf9a
kimbeomjun <gimb52043@gmail.com>
2023-04-09 01:09:03 +0900

    코인시세 정보

---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme : DefaultTheme = {
+    bgColor: "#02343F",
+    textColor: "#F0EDCC",
+    accentColor:"skyblue",
+}
+
+// export const darkTheme : DefaultTheme = {
+//     bgColor: "black",
+//     textColor: "white",
+    
+// }
